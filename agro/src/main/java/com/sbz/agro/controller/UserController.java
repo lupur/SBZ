151f7fac0fa510a70e0a1824bc40b6e605097f01
Sava Maric <sava.maric@hotmail.com>
2020-04-19 22:16:37 +0200

    API prefix

--- agro/src/main/java/com/sbz/agro/controller/UserController.java
+++ agro/src/main/java/com/sbz/agro/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sbz.agro.dto.UserRegistrationDto;
@@ -18,6 +19,7 @@ import com.sbz.agro.service.UserService;
 import com.sbz.agro.validator.UserValidator;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
