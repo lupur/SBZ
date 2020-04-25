bad04f7467ecb11eaca7b5746a947931d7437c34
lupur <milos.lupurovic@yahoo.com>
2020-04-25 21:01:12 +0200

    WIP: Device Array

--- agro/src/main/java/com/sbz/agro/controller/FieldController.java
+++ agro/src/main/java/com/sbz/agro/controller/FieldController.java
@@ -87,5 +87,4 @@ public class FieldController {
             return ResponseEntity.notFound().build();
         }
     }
-
 }
---
+++ agro/src/main/java/com/sbz/agro/service/DeviceArrayServiceImpl.java
@@ -0,0 +1,36 @@
+package com.sbz.agro.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sbz.agro.model.DeviceArray;
+
+@Service
+public class DeviceArrayServiceImpl implements DeviceArrayService {
+
+    @Override
+    public boolean addArray(Long fieldId) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean removeArray(Long fieldId) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public DeviceArray getDeviceArray(Long arrayId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<DeviceArray> getFieldArrays(Long fieldId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
