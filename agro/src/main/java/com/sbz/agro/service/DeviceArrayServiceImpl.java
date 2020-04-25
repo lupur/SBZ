33e539e7b2fc8c5ce5f5ee61d856bf48b2c386a8
Marina Simic <simic.marina@outlook.com>
2020-04-25 22:11:54 +0200

    Implemented Device Array Service.

--- agro/src/main/java/com/sbz/agro/service/DeviceArrayServiceImpl.java
+++ agro/src/main/java/com/sbz/agro/service/DeviceArrayServiceImpl.java
@@ -2,35 +2,68 @@ package com.sbz.agro.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sbz.agro.model.DeviceArray;
+import com.sbz.agro.model.*;
+import com.sbz.agro.repository.FieldRepository;
 
 @Service
 public class DeviceArrayServiceImpl implements DeviceArrayService {
+	
+	@Autowired
+	FieldRepository fieldRepository;
 
     @Override
     public boolean addArray(Long fieldId) {
-        // TODO Auto-generated method stub
-        return false;
+    	Field field = fieldRepository.findById(fieldId).get();
+    	
+        if(field == null)
+        	return false;
+        
+        DeviceArray dArray = new DeviceArray();
+        dArray.setField(field);
+        field.addDeviceArray(dArray);
+        
+        fieldRepository.save(field);
+        
+        return true;
     }
 
     @Override
-    public boolean removeArray(Long fieldId) {
-        // TODO Auto-generated method stub
+    public boolean removeArray(Long arrayId) {
+    	
+    	for (Field f : fieldRepository.findAll()){
+    		for (DeviceArray da : f.getDeviceArrays()){
+    			if (da.getId() == arrayId){
+    				f.getDeviceArrays().remove(da);
+    				fieldRepository.save(f);
+    				return true;
+    			}
+    		}
+    	}
+        
         return false;
     }
 
     @Override
     public DeviceArray getDeviceArray(Long arrayId) {
-        // TODO Auto-generated method stub
+    	
+    	for (Field f : fieldRepository.findAll()){
+    		for (DeviceArray da : f.getDeviceArrays()){
+    			if (da.getId() == arrayId){
+    				return da;
+    			}
+    		}
+    	}
+    	
         return null;
     }
 
     @Override
     public List<DeviceArray> getFieldArrays(Long fieldId) {
-        // TODO Auto-generated method stub
-        return null;
+
+        return fieldRepository.findById(fieldId).get().getDeviceArrays();
     }
 
 }
