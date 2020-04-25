3506fe7db7af5431b45fad3f31969ce1352e8da2
lupur <milos.lupurovic@yahoo.com>
2020-04-25 18:51:50 +0200

    Implemented Field Service - CRUD methods

--- agro/src/main/java/com/sbz/agro/service/FieldServiceImpl.java
+++ agro/src/main/java/com/sbz/agro/service/FieldServiceImpl.java
@@ -1,54 +1,140 @@
 package com.sbz.agro.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sbz.agro.dto.FieldDto;
+import com.sbz.agro.model.Crop;
+import com.sbz.agro.model.Field;
+import com.sbz.agro.model.User;
+import com.sbz.agro.repository.CropRepository;
+import com.sbz.agro.repository.FieldRepository;
+import com.sbz.agro.repository.UserRepository;
 
 @Service
 public class FieldServiceImpl implements FieldService {
 
+    @Autowired
+    FieldRepository fieldRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CropRepository cropRepository;
+
     @Override
     public List<FieldDto> getAllFields() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Field> fieldsDb = fieldRepository.findAll();
+        List<FieldDto> fields = new ArrayList<FieldDto>();
+        entityToDtoList(fieldsDb, fields);
+
+        return fields;
     }
 
     @Override
     public List<FieldDto> getUsersFields(String username) {
-        // TODO Auto-generated method stub
-        return null;
+        User u = userRepository.findByUsername(username);
+        if (u == null)
+            return null;
+
+        List<Field> fieldsDb = fieldRepository.findByOwner_Id(u.getId());
+        List<FieldDto> fields = new ArrayList<FieldDto>();
+
+        entityToDtoList(fieldsDb, fields);
+
+        return fields;
     }
 
     @Override
     public FieldDto getField(Long fieldId) {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            Field f = fieldRepository.findById(fieldId).get();
+            FieldDto field = new FieldDto();
+            field.setArea(f.getArea());
+            field.setId(f.getId());
+            field.setName(f.getName());
+            field.setOwnerId(f.getOwner().getId());
+            field.setCropId(f.getCrop().getId());
+            return field;
+        } catch (NoSuchElementException e) {
+            return null;
+        }
     }
 
     @Override
     public boolean createField(FieldDto newField) {
-        // TODO Auto-generated method stub
-        return false;
+        if (newField == null)
+            return false;
+
+        try {
+            User owner = userRepository.findById(newField.getOwnerId()).get();
+            Crop crop = cropRepository.findById(newField.getCropId()).get();
+            Field f = new Field();
+            f.setArea(newField.getArea());
+            f.setName(newField.getName());
+            f.setOwner(owner);
+            f.setCrop(crop);
+            fieldRepository.save(f);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean removeField(Long fieldId) {
-        // TODO Auto-generated method stub
-        return false;
+        try {
+            Field f = fieldRepository.findById(fieldId).get();
+            fieldRepository.delete(f);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean userOwnsField(String username, Long fieldId) {
-        // TODO Auto-generated method stub
-        return false;
+        User u = userRepository.findByUsername(username);
+        if (u == null)
+            return false;
+        try {
+            Field f = fieldRepository.findById(fieldId).get();
+            return f.getOwner().getId() == u.getId();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
     @Override
     public boolean fieldExists(Long fieldId) {
-        // TODO Auto-generated method stub
-        return false;
+        try {
+            fieldRepository.findById(fieldId).get();
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private void entityToDtoList(List<Field> fieldsDb, List<FieldDto> fieldsDto) {
+        if (fieldsDb == null || fieldsDto == null)
+            return;
+
+        for (Field f : fieldsDb) {
+            FieldDto field = new FieldDto();
+            field.setId(f.getId());
+            field.setArea(f.getArea());
+            field.setName(f.getName());
+            field.setOwnerId(f.getOwner().getId());
+            field.setCropId(f.getCrop().getId());
+            fieldsDto.add(field);
+        }
+
     }
 
 }
