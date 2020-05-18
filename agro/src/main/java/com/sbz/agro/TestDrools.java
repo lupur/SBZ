832e5aff3b0b139a0f6b993d60d139c148e09aae
Sava Maric <sava.maric@hotmail.com>
2020-05-18 02:12:39 +0200

    Fixing errors

--- agro/src/main/java/com/sbz/agro/TestDrools.java
+++ agro/src/main/java/com/sbz/agro/TestDrools.java
@@ -29,9 +29,9 @@ public class TestDrools {
     	
 		DeviceArray array1 = new DeviceArray();
 		array1.setId(1l);
-    	Device s12 = new Device("S12000", DeviceDetails.SENSOR, array1, 2);
+    	Device s12 = new Device("S12000", DeviceDetails.MOISTURE_SENSOR, array1, 2);
     	s12.setId(1l);
-    	Device s13 = new Device("S13000", DeviceDetails.SENSOR, array1, 3);
+    	Device s13 = new Device("S13000", DeviceDetails.MOISTURE_SENSOR, array1, 3);
     	s13.setId(2l);
     	Device v12 = new Device("V12000", DeviceDetails.VALVE, array1, 2);
     	v12.setId(4l);
@@ -41,15 +41,15 @@ public class TestDrools {
     	array1.setDevices(Arrays.asList(s12, v12, pump1));
     	
     	
-    	Reading r1 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading r111 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading ri = new Reading(v12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading r2 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.OK.name(), new Date().getTime());
-    	Reading r3 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading r4 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading r5 = new Reading(s13, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading rPump = new Reading(pump1, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date().getTime());
-    	Reading rPumpOk = new Reading(pump1, DeviceReadingTypes.STATUS.name(), ReadingValues.OK.name(), new Date().getTime());
+    	Reading r1 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading r111 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading ri = new Reading(v12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading r2 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.OK.name(), new Date());
+    	Reading r3 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading r4 = new Reading(s12, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading r5 = new Reading(s13, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading rPump = new Reading(pump1, DeviceReadingTypes.STATUS.name(), ReadingValues.ERROR.name(), new Date());
+    	Reading rPumpOk = new Reading(pump1, DeviceReadingTypes.STATUS.name(), ReadingValues.OK.name(), new Date());
     	
 //    	kieSession.insert(array1);
 //    	kieSession.fireAllRules();
@@ -123,11 +123,11 @@ public class TestDrools {
 	public static void insertDevices(KieSession kieSession) {
 		DeviceArray array1 = new DeviceArray();
 		array1.setId(1l);
-		Device s11 = new Device("S11000", DeviceDetails.SENSOR, array1, 1);
+		Device s11 = new Device("S11000", DeviceDetails.MOISTURE_SENSOR, array1, 1);
 		s11.setId(1l);
-		Device s12 = new Device("S12000", DeviceDetails.SENSOR, array1, 2);
+		Device s12 = new Device("S12000", DeviceDetails.MOISTURE_SENSOR, array1, 2);
 		s12.setId(2l);
-		Device s13 = new Device("S13000", DeviceDetails.SENSOR, array1, 3);
+		Device s13 = new Device("S13000", DeviceDetails.MOISTURE_SENSOR, array1, 3);
 		s13.setId(3l);
 		Device v11 = new Device("V11000", DeviceDetails.VALVE, array1, 1);
 		v11.setId(4l);
@@ -140,11 +140,11 @@ public class TestDrools {
 		
 		DeviceArray array2 = new DeviceArray();
 		array2.setId(2l);
-		Device s21 = new Device("S21000", DeviceDetails.SENSOR, array2, 1);
+		Device s21 = new Device("S21000", DeviceDetails.MOISTURE_SENSOR, array2, 1);
 		s21.setId(7l);
-		Device s22 = new Device("S22000", DeviceDetails.SENSOR, array2, 2);
+		Device s22 = new Device("S22000", DeviceDetails.MOISTURE_SENSOR, array2, 2);
 		s22.setId(8l);
-		Device s23 = new Device("S23000", DeviceDetails.SENSOR, array2, 3);
+		Device s23 = new Device("S23000", DeviceDetails.MOISTURE_SENSOR, array2, 3);
 		s23.setId(9l);
 		Device v21 = new Device("V21000", DeviceDetails.VALVE, array2, 1);
 		v21.setId(10l);
@@ -159,15 +159,15 @@ public class TestDrools {
 		field1.setId(1l);
 		field1.addDeviceArray(array1);
 		field1.setOwner(user);
-		field1.setMoistureLowerThreshold(Double.valueOf(20));
-		field1.setMoistureUpperThreshold(Double.valueOf(60));
+//		field1.setMoistureLowerThreshold(Double.valueOf(20));
+//		field1.setMoistureUpperThreshold(Double.valueOf(60));
 		array1.setField(field1);
 		
 		Field field2 = new Field();
 		field2.setId(2l);
 		field2.addDeviceArray(array2);
-		field2.setMoistureLowerThreshold(Double.valueOf(40));
-		field2.setMoistureUpperThreshold(Double.valueOf(80));
+//		field2.setMoistureLowerThreshold(Double.valueOf(40));
+//		field2.setMoistureUpperThreshold(Double.valueOf(80));
 		array2.setField(field2);
 		
 		kieSession.insert(s11);
