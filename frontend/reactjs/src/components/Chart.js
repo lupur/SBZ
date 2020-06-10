6bdb169ca6a33fc6c2c7207fed8ca684c2f76f2a
Sava Maric <sava.maric@hotmail.com>
2020-06-10 02:44:46 +0200

    Chart improvement

--- frontend/reactjs/src/components/Chart.js
+++ frontend/reactjs/src/components/Chart.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { VictoryAxis, VictoryLine, VictoryChart} from 'victory';
 import {readingService} from '../services/readingService'
 
-const data = [
-    {x: new Date(1590590718318), y: 13000},
-    {x: new Date(1590891718318), y: 16500},
-    {x: new Date(1591592718318), y: 14250},
-    {x: new Date(1591593718318), y: 19000}
-  ];
-
-
 class Chart extends React.Component {
 
   constructor(props) {
@@ -18,30 +10,30 @@ class Chart extends React.Component {
     this.deviceEUI = props.deviceEUI;
     this.readingType = props.readingType;
 
-    this.print = this.print.bind(this);
     this.getReadings = this.getReadings.bind(this);
   }  
 
   async componentDidMount() {
-    this.print();
     await this.getReadings();
   }
 
   async getReadings() {
     await readingService.getReadings(this.deviceEUI, this.readingType).then(response =>{
-      console.log(response);
+      this.setState({readings: response});
+      let data = [];
+      this.setState({data: data});
+      response.forEach(element => {
+        data.push({x: new Date(element.timestamp), y: parseFloat(element.value)})
+      });
+      this.setState({data: data});
+      setInterval(this.getReadings, 10000);
   });
-  }
-
-  print() {
-    console.log(this.deviceEUI);
-    console.log(this.readingType);
   }
 
   render() {
         return (
           <VictoryChart>
-                  <VictoryLine data={data} style={{data: { stroke: "#c43a31" }}} />
+             {this.state && this.state.data ? <VictoryLine data={this.state.data} style={{data: { stroke: "#c43a31" }}} /> : null}
           </VictoryChart>
         )
       }
